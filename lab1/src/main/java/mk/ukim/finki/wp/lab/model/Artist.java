68b09724748df30e6365bc18f821b73a913a56aa
gagal18 <gagaleskibojan@gmail.com>
2024-11-03 21:15:15 +0100

    feat: Full solution on Lab1

--- lab1/src/main/java/mk/ukim/finki/wp/lab/model/Artist.java
+++ lab1/src/main/java/mk/ukim/finki/wp/lab/model/Artist.java
@@ -1,4 +1,24 @@
 package mk.ukim.finki.wp.lab.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Random;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
 public class Artist {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String bio;
+    Random rd = new Random();
+
+    public Artist(String firstName, String lastName, String bio) {
+        this.id = Math.abs(rd.nextLong());
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.bio = bio;
+    }
 }
---
+++ lab1/src/main/java/mk/ukim/finki/wp/lab/repository/ArtistRepository.java
@@ -0,0 +1,17 @@
+package mk.ukim.finki.wp.lab.repository;
+import mk.ukim.finki.wp.lab.bootstrap.DataInit;
+import mk.ukim.finki.wp.lab.model.Artist;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ArtistRepository {
+    public List<Artist> findAll() {
+        return DataInit.artists;
+    }
+    public Optional<Artist> findById(Long id) {
+        return DataInit.artists.stream().filter(i -> i.getId().equals(id)).findFirst();
+    }
+}
